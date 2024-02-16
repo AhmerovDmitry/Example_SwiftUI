417a796a8b55d41afebefc0e49e54305089d4fbe
Ахмеров Дмитрий <DNAkhmerov@sberbank.ru>
2024-02-16 20:19:13 +0800

    initial commit

---
+++ Example_App_1/Example_App_1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Example_App_1
+//
+//  Created by Ахмеров Дмитрий Николаевич on 16.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example_App_1/Example_App_1/Example_App_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Example_App_1App.swift
+//  Example_App_1
+//
+//  Created by Ахмеров Дмитрий Николаевич on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Example_App_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

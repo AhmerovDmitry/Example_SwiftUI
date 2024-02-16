923945e42926857e5442b49e4769adee0f7e9363
Ахмеров Дмитрий <DNAkhmerov@sberbank.ru>
2024-02-16 22:18:26 +0800

    initial commit

---
+++ Example_App_2/Example_App_2/Example_App_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Example_App_2App.swift
+//  Example_App_2
+//
+//  Created by Ахмеров Дмитрий Николаевич on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Example_App_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
